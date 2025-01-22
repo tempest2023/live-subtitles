a8ce9ee4331626dd577c996b054250d56d5154b7
Tempest <623059008@qq.com>
2025-01-22 09:29:09 -0800

    Initial Commit

---
+++ liveSubtitle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  liveSubtitle
+//
+//  Created by RenTao on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ liveSubtitle/liveSubtitleApp.swift
@@ -0,0 +1,17 @@
+//
+//  liveSubtitleApp.swift
+//  liveSubtitle
+//
+//  Created by RenTao on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct liveSubtitleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
